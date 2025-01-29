764c3de1c9003730c8916b3ea9fa1811892858ad
lpinn <lep86@pitt.edu>
2025-01-29 13:26:27 -0500

    POST req

--- backend/controllers/transaction.controller.js
+++ backend/controllers/transaction.controller.js
@@ -2,16 +2,9 @@ import Transaction from '../models/transaction.model.js';
 
 export const postTransaction = async (req,res) => {
     const { amount, category, desc, method, recurring } = req.body;
- 
-    console.log("amount: " + amount);
-    console.log("category: " + category);
-    console.log("desc: " + desc);
-    console.log("method: " + method);
-    console.log("recurring: " + recurring);
-    console.log("date: " + new Date());
 
     if(!amount || !category || !desc || !method) {
-        return res.status(400).json({ success: false, message: "Please provide all fields" }); 
+        return res.status(400).json({ success: false, message: "Please provide all fields." }); 
     }
 
     const newTrx = new Transaction({
@@ -27,7 +20,7 @@ export const postTransaction = async (req,res) => {
         await newTrx.save(); 
         res.status(201).json({ success: true, data: newTrx });
     } catch (error) {
-        console.error("Error in posting trx: ", error); 
-        res.status(500).json({ success: false, message: "Server Error" });
+        console.error("Error in posting transaction: ", error); 
+        res.status(500).json({ success: false, message: "Server Error." });
     }
 };
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 import express from "express"; 
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js"; 
-import trxRoutes from "./routes/transaction.routes.js";
+import apiRoutes from "./routes/api.routes.js";
 
 dotenv.config(); 
 
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); //middleware - allows us to accept JSON data in the req body 
 
-app.use("/api/trx", trxRoutes); 
+app.use("/api", apiRoutes); 
 
 app.listen(5000, () => {
     connectDB();
